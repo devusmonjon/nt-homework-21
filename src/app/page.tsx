17fb8a629fde0fc41d887f055643bda9689eb55d
devusmonjon <usmonjonhasanov777@gmail.com>
2024-07-22 10:42:04 +0500

    add card icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -206,9 +206,75 @@ const Page = (props: Props) => {
 
                   return (
                     <div
-                      className='lg:w-[28%] md:w-[45%] sm:w-[48%] w-[100%] h-[440px] flex flex-col items-center mb-6'
+                      className='lg:w-[28%] md:w-[45%] sm:w-[48%] w-[100%] h-[440px] flex flex-col items-center mb-6 relative card'
                       key={product.id}
                     >
+                      <div className='absolute top-[10px] right-[15px] card-icons opacity-0'>
+                        <button className='w-[38px] h-[38px] rounded-full bg-[#3858D6] outline-none grid place-items-center'>
+                          <div className='w-[22px] h-[22px] grid place-items-center'>
+                            <svg
+                              width='22'
+                              height='22'
+                              viewBox='0 0 22 22'
+                              fill='none'
+                              xmlns='http://www.w3.org/2000/svg'
+                            >
+                              <g clip-path='url(#clip0_18_119)'>
+                                <path
+                                  d='M8.25 20.1667C8.75626 20.1667 9.16667 19.7563 9.16667 19.25C9.16667 18.7438 8.75626 18.3334 8.25 18.3334C7.74374 18.3334 7.33334 18.7438 7.33334 19.25C7.33334 19.7563 7.74374 20.1667 8.25 20.1667Z'
+                                  stroke='white'
+                                  strokeWidth='2'
+                                  strokeLinecap='round'
+                                  strokeLinejoin='round'
+                                />
+                                <path
+                                  d='M18.3333 20.1667C18.8396 20.1667 19.25 19.7563 19.25 19.25C19.25 18.7438 18.8396 18.3334 18.3333 18.3334C17.8271 18.3334 17.4167 18.7438 17.4167 19.25C17.4167 19.7563 17.8271 20.1667 18.3333 20.1667Z'
+                                  stroke='white'
+                                  strokeWidth='2'
+                                  strokeLinecap='round'
+                                  strokeLinejoin='round'
+                                />
+                                <path
+                                  d='M0.916664 0.916626H4.58333L7.04 13.1908C7.12382 13.6128 7.35341 13.9919 7.68858 14.2617C8.02375 14.5315 8.44314 14.6749 8.87333 14.6666H17.7833C18.2135 14.6749 18.6329 14.5315 18.9681 14.2617C19.3032 13.9919 19.5328 13.6128 19.6167 13.1908L21.0833 5.49996H5.5'
+                                  fill='white'
+                                />
+                                <path
+                                  d='M0.916664 0.916626H4.58333L7.04 13.1908C7.12382 13.6128 7.35341 13.9919 7.68858 14.2617C8.02375 14.5315 8.44314 14.6749 8.87333 14.6666H17.7833C18.2135 14.6749 18.6329 14.5315 18.9681 14.2617C19.3033 13.9919 19.5328 13.6128 19.6167 13.1908L21.0833 5.49996H5.5'
+                                  stroke='white'
+                                  strokeWidth='2'
+                                  strokeLinecap='round'
+                                  strokeLinejoin='round'
+                                />
+                              </g>
+                              <defs>
+                                <clipPath id='clip0_18_119'>
+                                  <rect width='22' height='22' fill='white' />
+                                </clipPath>
+                              </defs>
+                            </svg>
+                          </div>
+                        </button>
+                        <button className="w-[38px] h-[38px] rounded-full bg-[#FF6B6B] outline-none grid place-items-center mt-2">
+                          <div className="w-[23px] h-[21px] grid place-items-center">
+                            <svg
+                              width='23'
+                              height='21'
+                              viewBox='0 0 23 21'
+                              fill='none'
+                              xmlns='http://www.w3.org/2000/svg'
+                            >
+                              <path
+                                d='M19.5349 3.34542C19.0707 2.86826 18.5194 2.48975 17.9128 2.2315C17.3061 1.97325 16.6558 1.84033 15.9991 1.84033C15.3424 1.84033 14.6921 1.97325 14.0854 2.2315C13.4787 2.48975 12.9275 2.86826 12.4633 3.34542L11.4998 4.33522L10.5363 3.34542C9.5985 2.38205 8.32662 1.84083 7.00043 1.84083C5.67423 1.84083 4.40235 2.38205 3.46459 3.34542C2.52683 4.30879 2 5.6154 2 6.97781C2 8.34022 2.52683 9.64683 3.46459 10.6102L4.42808 11.6L11.4998 18.8648L18.5714 11.6L19.5349 10.6102C19.9994 10.1333 20.3678 9.567 20.6192 8.94374C20.8706 8.32048 21 7.65245 21 6.97781C21 6.30317 20.8706 5.63514 20.6192 5.01188C20.3678 4.38862 19.9994 3.82235 19.5349 3.34542Z'
+                                fill='white'
+                                stroke='white'
+                                strokeWidth='2'
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                              />
+                            </svg>
+                          </div>
+                        </button>
+                      </div>
                       <div className='w-full h-[337px] bg-[#F6F6F6] flex justify-center items-center overflow-hidden cursor-pointer'>
                         <div className='w-[250px] h-[280.032px] rounded-[25px] relative hover:scale-[1.2] transition-all duration-500 ease-[cubic-bezier(.47,0,.74,.71)]'>
                           <Image
@@ -351,9 +417,18 @@ const Page = (props: Props) => {
             >
               Get free guide about smart watches daily.
             </p>
-            <form onSubmit={(e) => e.preventDefault()} className='relative md:w-[443px] w-full'>
-              <input type='text' placeholder='Enter Email Address' className="w-full h-[60px] p-[19px_14px_20px_30px] rounded-[15px] outline-none" />
-              <button className="h-[45px] bg-[#3858D6] rounded-lg flex justify-center items-center text-white font-semibold md:mt-0 mt-4 md:w-[131px] w-full md:absolute top-[7px] right-[9px] outline-none border-[#3858d6] border-[2px] border-solid hover:bg-transparent hover:text-[#3858D6] transition-all duration-300">subscribe</button>
+            <form
+              onSubmit={(e) => e.preventDefault()}
+              className='relative md:w-[443px] w-full'
+            >
+              <input
+                type='text'
+                placeholder='Enter Email Address'
+                className='w-full h-[60px] p-[19px_14px_20px_30px] rounded-[15px] outline-none'
+              />
+              <button className='h-[45px] bg-[#3858D6] rounded-lg flex justify-center items-center text-white font-semibold md:mt-0 mt-4 md:w-[131px] w-full md:absolute top-[7px] right-[9px] outline-none border-[#3858d6] border-[2px] border-solid hover:bg-transparent hover:text-[#3858D6] transition-all duration-300'>
+                subscribe
+              </button>
             </form>
           </div>
           <div className='hidden md:block'>
